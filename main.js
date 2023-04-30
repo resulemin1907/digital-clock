34f687a06675530c51a58fc950d87c4d14dd683f
resulemin1907 <94853962+resulemin1907@users.noreply.github.com>
2023-04-30 23:41:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function GetTime() {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+
+    var day = now.getDate();
+    var month = now.getMonth() + 1;
+
+    hour < 10
+        ? (document.getElementById("hour").innerHTML = "0" + hour)
+        : (document.getElementById("hour").innerHTML = hour);
+
+    minute < 10
+        ? (document.getElementById("minute").innerHTML = "0" + minute)
+        : (document.getElementById("minute").innerHTML = minute);
+    second < 10
+        ? (document.getElementById("second").innerHTML = "0" + second)
+        : (document.getElementById("second").innerHTML = second);
+
+    document.getElementById("date").innerHTML =
+        day + " / " + month + " / " + year;
+}
+
+setInterval(function () {
+    GetTime();
+}, 1000);
